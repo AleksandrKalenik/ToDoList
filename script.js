d3db1f86baa3584bdadfd69231527d74d0dad5df
Aleksandr <102141924+AleksandrKalenik@users.noreply.github.com>
2022-09-12 14:40:15 +0300

    Add files via upload
    Добавлены часы в правом верхнем углу

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let textOfToDoList = ''
 console.table(toDoList)
 const addElementToDoNode = document.querySelector('.addToDoList')
 const showToDoNode = document.querySelector('.showToDoList')
+const addToDoTextInput = document.querySelector('.addToDoTextInput')
+const addToDoButton = document.querySelector('.submitAddToDoButton')
+
+// Метод установки фокуса на элементе INPUT для ввода
+const setFocusOnInput = () => {
+    document.getElementById('addToDoText').focus() 
+}
+
 
 //  Метод добавления элементов в список ToDo 
 const addToDoListElement = (textOfToDoList) => {
@@ -25,7 +33,7 @@ const refreshListOnPage = () => {
        
         //отображаются только элементы списка, статус которых - не завершён
         if (currentElement.isComplete === false) {
-            showToDoNode.innerHTML += `<ul>${currentElement.text} <button data-id='${currentElement.id}'>Выполнено</button></ul>`
+            showToDoNode.innerHTML += `<ul class="elementOfList">${currentElement.text} <button class="completeButtons" data-id='${currentElement.id}'>Выполнено</button></ul>`
             
 
         }
@@ -63,7 +71,9 @@ const refreshListOnPage = () => {
             changeStatusOfToDoElement(id)
         
         refreshListOnPage() //Обновляем список на странице
-
+        
+        // Переключить фокус на поле ввода   
+        setFocusOnInput()
     })
 
     // Метод обработки кликов по кнопке добавления ToDo
@@ -77,15 +87,87 @@ const refreshListOnPage = () => {
             return
 
         }
-        
+
         //добавляем новый пункт в массив
 
         const text = document.getElementById('addToDoText').value
-        addToDoListElement(text)
-        
-        //Обновляем список на странице
+        if (text != '') {
+            document.getElementById('addToDoText').value = ''
+            // Переключить фокус на поле ввода   
+            setFocusOnInput() 
+            addToDoListElement(text)
+            
+            //Обновляем список на странице
 
-        refreshListOnPage()
-
+            refreshListOnPage()
+        }
         
     })
+
+    // Метод обработки нажатия кнопки 'ENTER'
+
+    addToDoTextInput.addEventListener('keyup', (event) => {
+        
+        // если код нажатой клавиши = 13 - выполняется событие клика по кнопке добавления в список
+        if (event.keyCode === 13) {
+            addToDoButton.click()
+            
+        }
+    })
+
+    // var dateNow = new Date()
+    // Функция возвращает строку - текущее время (07:10:25)
+    const getTimeNow = () => {
+        var dateNow = new Date()
+        return `${addZeroNumber(dateNow.getHours())}:${addZeroNumber(dateNow.getMinutes())}:${addZeroNumber((dateNow.getSeconds()))}`
+    }
+
+    // Функция возвращает строку - текущую дату
+    const getDateNow = () => {
+        var dateNow = new Date()
+        return `${dateNow.getDate()} ${monthNumberToStr(dateNow.getMonth())} ${dateNow.getFullYear()}`
+    }
+
+    // Функция принимает числовое значение (0-11) и возвращает текстовое значение месяца. При ошибке аргумента - '--'
+    const monthNumberToStr = (monthNumber) => {
+        switch (monthNumber) {
+            case 0: return 'Января';
+            case 1: return 'Февраля';
+            case 2: return 'Марта';
+            case 3: return 'Апреля';
+            case 4: return 'Мая';
+            case 5: return 'Июня';
+            case 6: return 'Июля';
+            case 7: return 'Августа';
+            case 8: return 'Сентября';
+            case 9: return 'Октября';
+            case 10: return 'Ноября';
+            case 11: return 'Декабря';
+            default: return '--'
+        }
+    }
+    // Функция добавляет 0 впереди к целому числу от 1-9 и возвращает как строку
+    const addZeroNumber = (numberArg) => {
+        if (Math.trunc(numberArg) >= 0 && Math.trunc(numberArg) <= 9) {
+            return `0` + Math.trunc(numberArg)
+        }
+        else return String(Math.trunc(numberArg))
+    }
+
+    function showDate(htmlElementId) {
+        document.getElementById(htmlElementId).innerHTML = getDateNow()
+    }
+
+    function showTime(htmlElementId) {
+        document.getElementById(htmlElementId).innerHTML = getTimeNow()
+    }
+
+    // Функция обновляет время и дату на странице
+    function outText () {
+        showTime('time')
+        showDate('date')
+    }
+
+    // Задаём интервал обновления времени и даты на странице
+    setInterval(outText, 1000)
+
